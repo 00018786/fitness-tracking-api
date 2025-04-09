f28b47b05709d60d9adac3e9d3d9392d189ce883
Izzat Sotimkulov <isotimkulov@adnoc.ae>
2025-04-09 21:49:30 +0500

    json-server fixes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: './public' // Provide a path for static files
+});
 const port = process.env.PORT || 3001;
+
+// Create public directory if it doesn't exist
+const fs = require('fs');
+if (!fs.existsSync('./public')) {
+  fs.mkdirSync('./public');
+}
 
 server.use(middlewares);
 server.use(router);
